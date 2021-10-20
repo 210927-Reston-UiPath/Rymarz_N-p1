d926cc3d8dc405020ef7ef9aeb945ee5887b4733
Nathan Rymarz <nrymarz@gmail.com>
2021-10-20 18:04:26 -0500

    automating add products to cart from custom store

--- p1Db.sql
+++ p1Db.sql
@@ -1,13 +1,13 @@
-create table items(
-    id serial primary key,
-    name VARCHAR(100),
-    price Numeric(100,2)
-);
-
 create table vendors(
     id serial primary KEY,
     name VARCHAR(30),
     website VARCHAR(100)
+);
+
+create table items(
+    id serial primary key,
+    name VARCHAR(100),
+    vendorId integer references vendors(id)
 );
 
 create table clients(
@@ -15,26 +15,24 @@ create table clients(
     name varchar(30)
 );
 
-create table Inventory(
-    vendorId integer REFERENCES vendors(id),
-    itemId integer REFERENCES items(id)
-);
-
 create table clientItems(
     clientId integer references clients(id),
-    itemId integer references items(id)
+    itemId integer references items(id),
+    quantity integer
 );
-
-insert into items(name,price) VALUES
-    ('sirop derable',28.5)
 
 select * from items;
 select * from vendors;
 select * from clients;
+select * from clientItems;
 
-drop table Inventory;
 drop table clientItems;
 drop table items;
 drop table vendors;
 drop table clients;
+
+select clients.NAME as client, items.name as Item, clientItems.quantity, vendors.website from clients join clientItems 
+on clientItems.clientId = id join items
+on clientItems.itemId = items.id join vendors 
+on items.vendorId = vendors.id
 
