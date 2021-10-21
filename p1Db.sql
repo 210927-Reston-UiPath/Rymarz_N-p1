7f9cde18b4a5ddb8519278a8a5656a820d42e970
Nathan Rymarz <nrymarz@gmail.com>
2021-10-21 18:45:20 -0500

    worked on a bunch of stuff

--- p1Db.sql
+++ p1Db.sql
@@ -16,23 +16,55 @@ create table clients(
 );
 
 create table clientItems(
+    id serial primary KEY,
     clientId integer references clients(id),
     itemId integer references items(id),
     quantity integer
 );
 
+create table expenses(
+    orderId integer references clientItems(id),
+    subtotal numeric(100,2),
+    orderDate date
+);
+
+create table failedOrders(
+    orderId integer references clientItems(id),
+    orderDate date
+)
+
+
 select * from items;
 select * from vendors;
 select * from clients;
 select * from clientItems;
+select * from expenses;
+select * from failedOrders
 
+select clients.name as "Client",orderDate, clientItems.itemid from failedOrders join clientItems
+on orderId = clientItems.id join clients
+on clientItems.clientId = clients.id
+
+select clients.NAME as Client, sum(subtotal) as TotalPrice from expenses join clientitems
+on orderId = clientItems.id join clients
+on clientItems.clientId = clients.Id
+group by client
+
+select clients.name as client, subtotal from expenses join clientItems
+on orderId = clientItems.id join clients
+on clientItems.clientId = clients.id
+
+
+drop table expenses;
+drop table failedOrders;
 drop table clientItems;
 drop table items;
 drop table vendors;
 drop table clients;
 
-select clients.NAME as client, items.name as Item, clientItems.quantity, vendors.website from clients join clientItems 
-on clientItems.clientId = id join items
+
+select clients.NAME as client, items.name as Item, clientItems.quantity, vendors.website, clientItems.id from clients join clientItems 
+on clientItems.clientId = clients.id join items
 on clientItems.itemId = items.id join vendors 
 on items.vendorId = vendors.id
 
